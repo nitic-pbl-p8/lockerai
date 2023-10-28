6284db3990dd6a319cd8463c7f709e7b47a59f1a
shio <85730998+dino3616@users.noreply.github.com>
2023-10-28 16:54:28 +0900

    feat: ✨ (user/dto/user-create) avatarUrl を作成

--- apps/api/src/module/user/controller/dto/input/user-create.input.ts
+++ apps/api/src/module/user/controller/dto/input/user-create.input.ts
@@ -1,8 +1,14 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsUUID, IsUrl } from 'class-validator';
 import type { User } from '#api/module/user/domain/user.model';
 
 @InputType()
 export class UserCreateInput implements Omit<User, 'id' | 'fingerprintId' | 'lostAndFoundState' | 'createdAt'> {
   @Field(() => String, { nullable: false })
+  @IsUUID()
   authId!: string;
+
+  @Field(() => String, { nullable: false })
+  @IsUrl()
+  avatarUrl!: string;
 }
