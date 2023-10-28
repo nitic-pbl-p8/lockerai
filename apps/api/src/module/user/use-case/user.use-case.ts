4bf8c2312247af0882c1bae5298967ffc6bcea3b
shio <85730998+dino3616@users.noreply.github.com>
2023-10-28 16:51:57 +0900

    feat: ✨ (user/use-case) verifyUserPresence を作成

--- apps/api/src/module/user/use-case/user.use-case.ts
+++ apps/api/src/module/user/use-case/user.use-case.ts
@@ -1,6 +1,6 @@
 import type { User } from '#api/module/user/domain/user.model';
 
 export interface UserUseCaseInterface {
-  findUser(userId: User['id']): Promise<User | null>;
   createUser(user: Omit<User, 'id' | 'fingerprintId' | 'lostAndFoundState' | 'createdAt'>): Promise<User>;
+  verifyUserPresence(authId: User['authId']): Promise<User | null>;
 }
