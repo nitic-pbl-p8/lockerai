cd3cd52cf6d5fc14de828e955bfef6d375a85da6
shio <85730998+dino3616@users.noreply.github.com>
2023-10-28 20:59:34 +0900

    fix: 🐛 (github/workflow) gql:introspect が正常に filter されるように修正

--- .github/workflows/script/graphql-commit-schema.sh
+++ .github/workflows/script/graphql-commit-schema.sh
@@ -3,5 +3,5 @@
 if [ "$1" = 1 ]; then
     pnpm api dev
 elif [ "$1" = 2 ]; then
-    sleep 15 && pnpm --filter='@shio/website' gql:introspect
+    sleep 15 && pnpm --filter='@lockerai/website' gql:introspect
 fi
