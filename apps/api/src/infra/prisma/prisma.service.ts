08c2ac197ebf257f17298d1830f8d4806b2108aa
shio <85730998+dino3616@users.noreply.github.com>
2023-11-28 09:17:12 +0900

    feat: ✨ (api) prisma module を作成

---
+++ apps/api/src/infra/prisma/prisma.service.ts
@@ -0,0 +1,29 @@
+import { Inject, Injectable, Logger, type OnModuleDestroy, type OnModuleInit } from '@nestjs/common';
+import { type Prisma, PrismaClient } from '@prisma/client';
+import { match } from 'ts-pattern';
+import { EnvService } from '#api/common/service/env/env.service';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor(@Inject(EnvService) readonly envService: EnvService) {
+    const log = match<(typeof envService)['NodeEnv'], Prisma.LogLevel[]>(envService.NodeEnv)
+      .with('development', () => ['query', 'info', 'warn', 'error'])
+      .with('production', () => ['query', 'info', 'warn', 'error'])
+      .with('test', () => ['info', 'warn', 'error'])
+      .otherwise(() => ['query', 'info', 'warn', 'error']);
+
+    super({ log });
+
+    this.logger.debug(`${PrismaService.name} constructed`);
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
