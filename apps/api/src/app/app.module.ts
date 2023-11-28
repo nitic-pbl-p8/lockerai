f3c846ec4ad4c785a1b2b35d533181c80af7bfee
shio <85730998+dino3616@users.noreply.github.com>
2023-11-28 09:21:48 +0900

    feat: 🎈 (api/user/repository) orm に prisma を使用するように修正

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { CacheModule } from '#api/common/service/cache/cache.module';
 import { EnvModule } from '#api/common/service/env/env.module';
 import { PubSubModule } from '#api/common/service/pubsub/pubsub.module';
 import { GraphQLConfigModule } from '#api/config/graphql/graphql-config.module';
-import { DrizzleModule } from '#api/infra/drizzle/drizzle.module';
+import { PrismaModule } from '#api/infra/prisma/prisma.module';
 import { Modules } from '#api/module';
 
 @Module({
-  imports: [EnvModule, GraphQLConfigModule, DrizzleModule, CacheModule, PubSubModule, ...Modules],
+  imports: [EnvModule, GraphQLConfigModule, PrismaModule, CacheModule, PubSubModule, ...Modules],
 })
 export class AppModule {}
--- apps/api/src/module/user/repository/impl/user.repository.ts
+++ apps/api/src/module/user/repository/impl/user.repository.ts
@@ -1,28 +1,23 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { eq } from 'drizzle-orm';
-import { PG_CONNECTION, type PgConnection } from '#api/infra/drizzle/drizzle.module';
-import { users } from '#api/infra/drizzle/schema';
+import { PrismaService } from '#api/infra/prisma/prisma.service';
 import { User } from '#api/module/user/domain/user.model';
 import type { UserRepositoryInterface } from '#api/module/user/repository/user.repository';
 
 @Injectable()
 export class UserRepository implements UserRepositoryInterface {
-  constructor(@Inject(PG_CONNECTION) private readonly conn: PgConnection) {}
+  constructor(@Inject(PrismaService) private readonly prismaService: PrismaService) {}
 
   async findByAuthId(authId: Parameters<UserRepositoryInterface['findByAuthId']>[0]): Promise<User | null> {
-    const foundUser = (await this.conn.select().from(users).where(eq(users.authId, authId))).at(0);
-    if (foundUser === undefined) {
+    const user = await this.prismaService.user.findUnique({ where: { authId } });
+    if (!user) {
       return null;
     }
 
-    return new User(foundUser);
+    return new User(user);
   }
 
   async create(user: Parameters<UserRepositoryInterface['create']>[0]): Promise<User> {
-    const createdUser = (await this.conn.insert(users).values(user).returning()).at(0);
-    if (createdUser === undefined) {
-      throw new Error('Failed to create user');
-    }
+    const createdUser = await this.prismaService.user.create({ data: user });
 
     return new User(createdUser);
   }
