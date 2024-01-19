1fcbb40abc597b8dc755067b17f547b37ef3dda4
shio <85730998+dino3616@users.noreply.github.com>
2024-01-19 11:46:56 +0900

    feat: ✨ (core/util/format-date) pattern に MMM. dd, yyyy HH:mm を追加

--- packages/core/util/format-date.ts
+++ packages/core/util/format-date.ts
@@ -9,6 +9,7 @@ type Pattern =
   | 'yyyy/MM/dd HH:mm'
   | 'yyyy/MM/dd'
   | 'MMM. dd, yyyy'
+  | 'MMM. dd, yyyy HH:mm'
   | 'MMMM dd, yyyy';
 
 export const formatDate = (date: Date, pattern: Pattern) => format(utcToZonedTime(date, 'Asia/Tokyo'), pattern);
