63c2fe29f4848c4630975ff4c8d26e8633234461
shio <85730998+dino3616@users.noreply.github.com>
2024-01-19 11:46:12 +0900

    feat: ✨ (urql) authExchange を追加

--- apps/website/src/infra/urql/ssr.tsx
+++ apps/website/src/infra/urql/ssr.tsx
@@ -2,11 +2,36 @@
 
 import { getBaseUrl } from '@lockerai/core/util/get-base-url';
 import { createUrqlClient } from '@lockerai/urql';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { UrqlProvider as UrqlProviderPrimitive } from '@urql/next';
 import type { ComponentPropsWithoutRef, ReactNode } from 'react';
 import schema from '~website/graphql.schema.json';
 
-const { urqlClient, ssr } = createUrqlClient(schema, getBaseUrl({ app: 'api' }).toString(), getBaseUrl({ app: 'api-ws' }).toString());
+const { urqlClient, ssr } = createUrqlClient(
+  schema,
+  getBaseUrl({ app: 'api' }).toString(),
+  getBaseUrl({ app: 'api-ws' }).toString(),
+  async () => {
+    const supabase = createClientComponentClient();
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+    if (!session) {
+      return null;
+    }
+
+    return [session.access_token, session.expires_at];
+  },
+  async () => {
+    const supabase = createClientComponentClient();
+    const {
+      data: { session },
+    } = await supabase.auth.refreshSession();
+    if (!session) {
+      supabase.auth.signOut();
+    }
+  },
+);
 
 type UrqlProviderProps = Omit<ComponentPropsWithoutRef<typeof UrqlProviderPrimitive>, 'client' | 'ssr'>;
 
--- packages/urql/index.ts
+++ packages/urql/index.ts
@@ -1,10 +1,17 @@
 import { type Client, cacheExchange, createClient, fetchExchange, ssrExchange } from '@urql/core';
 import { devtoolsExchange } from '@urql/devtools';
 import { registerUrql as registerUrqlByMaker } from '@urql/next/rsc';
+import { createAuthExchange } from './exchange/auth-exchange';
 import { createScalarExchamge } from './exchange/scalar-exchange';
 import { createSubscriptionExchange } from './exchange/subscription-exchange';
 
-export const createUrqlClient = (schema: unknown, graphqlUrl: string, wsUrl: string) => {
+export const createUrqlClient = (
+  schema: unknown,
+  graphqlUrl: string,
+  wsUrl: string,
+  getSession: () => Promise<[string, number | undefined] | null>,
+  refreshSession: () => Promise<void>,
+) => {
   const ssr = ssrExchange();
 
   const urqlClient = createClient({
@@ -13,6 +20,7 @@ export const createUrqlClient = (schema: unknown, graphqlUrl: string, wsUrl: str
       devtoolsExchange,
       createScalarExchamge(schema as Parameters<typeof createScalarExchamge>[0]),
       cacheExchange,
+      createAuthExchange(getSession, refreshSession),
       createSubscriptionExchange(wsUrl),
       ssr,
       fetchExchange,
@@ -27,8 +35,10 @@ export const registerUrql = (
   schema: Parameters<typeof createUrqlClient>[0],
   graphqlUrl: Parameters<typeof createUrqlClient>[1],
   wsUrl: Parameters<typeof createUrqlClient>[2],
+  getSession: Parameters<typeof createUrqlClient>[3],
+  refreshSession: Parameters<typeof createUrqlClient>[4],
 ) => {
-  const { urqlClient } = createUrqlClient(schema, graphqlUrl, wsUrl);
+  const { urqlClient } = createUrqlClient(schema, graphqlUrl, wsUrl, getSession, refreshSession);
   const { getClient } = registerUrqlByMaker(() => urqlClient);
 
   return { getClient };
