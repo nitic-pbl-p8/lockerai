b1042e399c2f54c8726173f9d86e778d9e87eaaa
shio <85730998+dino3616@users.noreply.github.com>
2024-01-19 11:31:38 +0900

    chore: 🔧 (next) qqxrhrpybdpxlwirjhaw.supabase.co の外部画像を許可するように修正

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -6,7 +6,12 @@ const config = {
   images: {
     remotePatterns: [
       {
+        protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'qqxrhrpybdpxlwirjhaw.supabase.co',
       },
     ],
   },
