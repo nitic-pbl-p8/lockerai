664c0856dc986fbcdefc3e5730a49af45ddb6011
shio <85730998+dino3616@users.noreply.github.com>
2024-01-23 09:41:46 +0900

    feat: ✨ (locker-dashboard/domain) LockerStatus domain を作成

--- apps/locker-dashboard/src/common/model/locker.ts
+++ apps/locker-dashboard/src/common/model/locker.ts
@@ -4,6 +4,14 @@ export type Locker = {
   createdAt: Date;
 };
 
+type StatusType = 'INFO' | 'ERROR' | 'WARN' | 'SUCCESS';
+
+export type LockerStatus = {
+  type: StatusType;
+  name: string;
+  description: string;
+};
+
 export const mockLocker = (locker: Partial<Locker> = {}): Locker => ({
   id: 'e069eeb2-a239-44c7-9870-acc1af492264',
   name: 'primary',
