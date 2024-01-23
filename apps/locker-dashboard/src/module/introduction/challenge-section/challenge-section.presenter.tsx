6a99c4f9a73115b3c29682ec18ee7eae14be1cde
shio <85730998+dino3616@users.noreply.github.com>
2024-01-23 17:01:00 +0900

    typo: 🖋️ (locker-dashboard/challenge-section) register の表現を link に修正

--- apps/locker-dashboard/src/module/introduction/challenge-section/challenge-section.presenter.tsx
+++ apps/locker-dashboard/src/module/introduction/challenge-section/challenge-section.presenter.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useTheme } from '#core/component/theme-provider';
-import { getBaseUrl } from '#core/util/get-base-url';
+import { useTheme } from '@lockerai/core/component/theme-provider';
+import { getBaseUrl } from '@lockerai/core/util/get-base-url';
 import { colors } from '@lockerai/design-token';
 import { AnimatePresence, motion } from 'framer-motion';
 import { QRCodeSVG } from 'qrcode.react';
@@ -36,15 +36,15 @@ export const ChallengeSection = ({ lockerId, fallback, ...props }: ChallengeSect
           {...props}
         >
           <hgroup className="flex flex-col items-center gap-6">
-            <h1 className="text-6xl font-bold text-sage-12">Register your fingerprint</h1>
+            <h1 className="text-6xl font-bold text-sage-12">Link your fingerprint</h1>
             <p className="text-2xl text-sage-11">
-              Your fingerprint is not yet registered.
+              Your fingerprint is not yet linked to your account.
               <br />
-              Scan the following QR code to register your fingerprint.
+              Scan the following QR code to link.
             </p>
           </hgroup>
           <QRCodeSVG
-            value={`${getBaseUrl({ app: 'website' })}/auth/challenge?lockerId=${lockerId}&hashedFingerprintId=${challengingHashedFingerprintId}`}
+            value={`${getBaseUrl({ app: 'website' }).toString()}auth/challenge?lockerId=${lockerId}&hashedFingerprintId=${challengingHashedFingerprintId}`}
             bgColor={match(resolvedTheme)
               .with('dark', () => colors.dark.green['2'])
               .with('light', () => colors.light.green['2'])
