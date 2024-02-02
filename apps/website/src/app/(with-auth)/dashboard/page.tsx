7605cfb5a0acc810d28faef1ebed75a2f7b11f73
shio <85730998+dino3616@users.noreply.github.com>
2024-02-02 12:56:27 +0900

    feat: ✨ (website/dashboard) fallback ui を作成

--- apps/website/src/app/(with-auth)/dashboard/page.tsx
+++ apps/website/src/app/(with-auth)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from '@lockerai/core/icon/check-icon';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import type { Metadata, NextPage } from 'next';
 import { cookies } from 'next/headers';
@@ -34,7 +35,15 @@ const DashboardPage: NextPage = async () => {
         'lost-and-found-state': foundUser.lostAndFoundState,
       }}
     />
-  ) : null;
+  ) : (
+    <h1 className="flex flex-col items-center p-5 py-10">
+      <span className="relative">
+        <CheckIcon className="h-10 w-10 fill-green-11" />
+        <CheckIcon aria-hidden className="absolute left-1/2 top-1/2 h-10 w-10 -translate-x-1/2 -translate-y-1/2 fill-green-11 blur-sm" />
+      </span>
+      <span className="text-center text-4xl font-bold text-sage-12">You have no task</span>
+    </h1>
+  );
 };
 
 export default DashboardPage;
