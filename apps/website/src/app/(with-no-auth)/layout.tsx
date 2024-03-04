ad785d9de9630a084b62e67d46325180fec19e92
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-03-04 17:24:45 +0900

    refactor: ♻️ ヘッダーを認証済みかどうかで切り替えるようにした (#7)

--- apps/website/src/app/(with-no-auth)/layout.tsx
+++ apps/website/src/app/(with-no-auth)/layout.tsx
@@ -2,16 +2,14 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import type { NextPage } from 'next';
 import { cookies } from 'next/headers';
 import type { ReactNode } from 'react';
-import { InAppHeader } from '#website/layout/global/header';
-import { AuthGuardProvider } from '#website/layout/with-auth/auth-guard-provider';
+import { Header } from '#website/layout/global/header';
 import { findUserUseCase } from '#website/use-case/find-user';
 
-type WithAuthLayoutProps = {
+type WithNoAuthLayoutProps = {
   children: ReactNode;
-  routeIndicator: ReactNode;
 };
 
-const WithAuthLayout: NextPage<WithAuthLayoutProps> = async ({ children, routeIndicator }) => {
+const WithNoAuthLayout: NextPage<WithNoAuthLayoutProps> = async ({ children }) => {
   // HACK: To avoid next build errors, functions that depend on async contexts need to be called outside the function that creates the new execution context.
   // ref: https://nextjs.org/docs/messages/dynamic-server-error
   const cookieStore = cookies();
@@ -25,11 +23,10 @@ const WithAuthLayout: NextPage<WithAuthLayoutProps> = async ({ children, routeIn
 
   return (
     <>
-      <InAppHeader user={foundUser}>{routeIndicator}</InAppHeader>
-      <AuthGuardProvider />
+      <Header user={foundUser} />
       {children}
     </>
   );
 };
 
-export default WithAuthLayout;
+export default WithNoAuthLayout;
